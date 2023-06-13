05aadf0cfc3caf0b94acc28195c67ddbc92a66d8
miuka <d.lolimiuku@gmail.com>
2023-06-14 01:12:55 +0900

    format file

--- config.h
+++ config.h
@@ -8,19 +8,17 @@
 /* appearance */
 static unsigned int borderpx = 4;               /* border pixel of windows */
 static unsigned int snap = 32;                  /* snap pixel */
-static const unsigned int systraypinning =
-    0;                                      /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst =
-    1;                                     /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray = 1;               /* 0 means no systray */
 static unsigned int gappih = 20;                /* horiz inner gap between windows */
 static unsigned int gappiv = 20;                /* vert inner gap between windows */
 static unsigned int gappoh = 20;                /* horiz outer gap between windows and screen edge */
 static unsigned int gappov = 30;                /* vert outer gap between windows and screen edge */
-static int swallowfloating = 0;                 /* 1 means swallow floating windows by default */
 static int smartgaps = 0;                       /* 1 means no outer gap when there is only one window */
+static int swallowfloating = 0;                 /* 1 means swallow floating windows by default */
 static int showbar = 1;                         /* 0 means no bar */
 static int topbar = 1;                          /* 0 means bottom bar */
 static char *fonts[] = { "monospace:size=12", "WenQuanYi Zen Hei Mono:size=12:type=Regular:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=22:antialias=true:autohint=true" };
@@ -90,6 +88,7 @@ static int resizehints = 0;     /* 1 means respect size hints in tiled resizals 
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 #define FORCE_VSPLIT 1          /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",    tile                   }, /* Default: Master on left, slaves on right */
