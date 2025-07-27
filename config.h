e107e716c611000b1436bdfc77fff79b8ed9831f
miuka <d.lolimiuku@gmail.com>
2025-07-27 17:40:21 +0900

    update config rule

--- config.h
+++ config.h
@@ -73,6 +73,8 @@ static const Rule rules[] = {
     {NULL,               NULL,     "Figure",          0,        1,        0,            1,            0,          0,        -1 },
     {NULL,               NULL,     "HG_Peer_OffScreenWindow", 0,1,        0,            1,            1,          1,        -1 },
     {"ij-ImageJ",        NULL,     NULL,              0,        1,        0,            1,            0,          0,        -1 },
+    {"Gnuplot",          NULL,     NULL,              0,        1,        0,            1,            0,          0,        -1 },
+    {"gnuplot_qt",       NULL,     NULL,              0,        1,        0,            1,            0,          0,        -1 },
     {"gui-twod-ChemDoodleLauncher", NULL,     NULL,   0,        1,        0,            1,            0,          0,        -1 },
     {"fiji-Main",        NULL,     NULL,              0,        1,        0,            1,            0,          0,        -1 },
     {"TelegramDesktop",  NULL,     NULL,              0,        1,        0,            1,            0,          0,        -1 },
